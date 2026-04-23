6dcec77b51f993406ebb78dc25937dd3cc9e79a4
Brigithe Correa <136396900+BrigitheCarolina022@users.noreply.github.com>
2026-04-22 19:18:30 -0500

    Update

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,4 @@
-#Este aplicativo tiene un dashboard para visualizar CPU, RAM, núcleos y sistema en tiempo real. Incluye procesamiento de imágenes y un log de ejecución donde verás cuánto tiempo tardó cada modelo de concurrencia en procesar cada imagen
-#El log también muestra información sobre la imagen más rápida y el estado de la RAM y CPU antes y después de la ejecución. Al finalizar los tres modelos, aparecerá una tabla comparativa de consumo. Se sugiere correrlo en Spyder 6 para no instalar nada extra, te recomiiendo crear una carpeta para cada modelo
+
 
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
